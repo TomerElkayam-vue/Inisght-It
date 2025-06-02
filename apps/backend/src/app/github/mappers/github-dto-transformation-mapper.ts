46351a705b0e22c953704a5826323b6d9982dbd6
shacharshemesh <shacharsms10@gmail.com>
2025-06-03 01:36:53 +0300

    fix

--- apps/backend/src/app/github/mappers/github-dto-transformation-mapper.ts
+++ apps/backend/src/app/github/mappers/github-dto-transformation-mapper.ts
@@ -1,6 +1,7 @@
-import { GithubDataType } from '../enums/github-data-type.enum'
+import { GithubDataType } from '../enums/github-data-type.enum';
 
-export const GithubDtoTransformationMapper: Record<GithubDataType,
+export const GithubDtoTransformationMapper: Record<
+  GithubDataType,
   {
     sprintInitaliztionValue: any;
     dataTransformation: (currentData: any, fields: any) => any;
@@ -8,32 +9,26 @@ export const GithubDtoTransformationMapper: Record<GithubDataType,
 > = {
   [GithubDataType.PR]: {
     sprintInitaliztionValue: 0,
-    dataTransformation: (currentData: any, fields: any) => {
-      return currentData + 1;
-    },
+    dataTransformation: (currentData: any, fields: any) => currentData + 1,
   },
   [GithubDataType.COMMENTS]: {
     sprintInitaliztionValue: 0,
-    dataTransformation: (currentData: any, fields: any) => {
-      return currentData + fields.review_comments;
-    },
+    dataTransformation: (currentData: any, fields: any) =>
+      currentData + fields.review_comments,
   },
   [GithubDataType.COMMITS]: {
     sprintInitaliztionValue: 0,
-    dataTransformation: (currentData: any, fields: any) => {
-      return currentData + fields.commits;
-    },
+    dataTransformation: (currentData: any, fields: any) =>
+      currentData + fields.commits,
   },
   [GithubDataType.FILE_CHANGES]: {
     sprintInitaliztionValue: {
-      additions : 0,
-      deletions: 0
+      additions: 0,
+      deletions: 0,
     },
-    dataTransformation: (currentData: any, fields: any) => {
-      return {
-        additions : currentData.additions + fields.additions,
-        deletions: currentData.deletions + fields.deletions,
-      }
-    },
-  }
+    dataTransformation: (currentData: any, fields: any) => ({
+      additions: currentData.additions + fields.additions,
+      deletions: currentData.deletions + fields.deletions,
+    }),
+  },
 };
