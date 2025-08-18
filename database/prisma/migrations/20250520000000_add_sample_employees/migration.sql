5cd9f488c17d699e57ae353f43e0d7fd3305894d
Ron Aruch <323725416@idf.il>
2025-08-18 23:35:37 +0300

    add roles and employee tables, update project_permissions schema, and modify package.json scripts

--- database/prisma/migrations/20250520000000_add_sample_employees/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-INSERT INTO "employee" ("display_name", "github_username", "jira_username") VALUES
-    ('Noan Ifat', 'noam11111', 'Noam'),
-    ('Nitzan Hamzani', 'nitzanh2000', 'Nitzan Hamzani'),
-    ('Shachar Shemesh', 'shacharshemesh', 'Shachar Shemesh'),
-    ('Ron Aruch', 'ron80098', 'ron80098'),
-    ('Tomer Elkayam', 'TomerElkayam-vue', 'Tomer Elkayam'); 
